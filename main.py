66c98b4d2a5b8726305cab7d97464404dadf1536
Genkun <905811049@qq.com>
2023-08-22 19:07:04 +0800

    Python tutorial for Verb

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# 这是一个示例 Python 脚本。
+# 1.1变量
+### 变量定义和赋值
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+# 在Python中，变量不需要显式声明类型。当你给变量赋值时，Python会自动确定变量的类型。
 
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+name = "Alice"  # 字符串类型
+age = 25  # 整数类型
+height = 1.68  # 浮点数类型
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+### 变量命名规则
+
+# 变量名称可以包含字母、数字和下划线，但不能以数字开头。
+
+my_name = "Bob"
+age1 = 30
+
+### 变量类型
+
+# 可以使用`type()`函数来查看变量的类型。
+print(type(name))  # <class 'str'>
+print(type(age))  # <class 'int'>
+print(type(height))  # <class 'float'>
+
+### 动态类型
+
+# Python是一种动态类型语言，这意味着你可以更改变量的类型。例如：
+x = 5
+print(type(x))  # <class 'int'>
+x = "hello"
+print(type(x))  # <class 'str'>
+
+### 多变量赋值
+
+# 可以一次为多个变量赋值。
+a, b, c = 5, 3.2, "Hello"
+
+# 或者为多个变量赋相同的值：
+x = y = z = "same"
